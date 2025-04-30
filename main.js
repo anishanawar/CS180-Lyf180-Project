2fb498f6c95b3b52080c5d380a60de346b214090
Sookie Drabla <sookie.sd@@gmail.com>
2025-04-30 14:52:16 -0700

    goal add button

---
+++ main.js
@@ -0,0 +1,11 @@
+function on() {
+    document.getElementById("add-goal-overlay").style.display = "block";
+}
+
+function off() {
+    document.getElementById("add-goal-overlay").style.display = "none";
+}
+
+
+
+
