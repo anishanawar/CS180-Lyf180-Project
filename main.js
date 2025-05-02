e4805cd78faffea8bf8fa8b40a866b68c7cc4432
Sookie Drabla <sookie.sd@@gmail.com>
2025-05-02 16:42:16 -0700

    add goal list and clear

--- main.js
+++ main.js
@@ -6,6 +6,17 @@ function off() {
     document.getElementById("add-goal-overlay").style.display = "none";
 }
 
+function addGoal() {
+    const goalInput = document.getElementById("goal-input").value;
+    const goalList = document.getElementById("goal-list");
+    const goalItem = document.createElement("li");
+    goalItem.textContent = goalInput;
+    goalList.appendChild(goalItem);
+    document.getElementById("goal-input").value = "";
+}
 
-
+function clearGoals() {
+    const goalList = document.getElementById("goal-list");
+    goalList.innerHTML = "";
+}
 
